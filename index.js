883c4d05d87d407a6e8440a3ae4448eced502127
RArgollo <rafaelargollo22@hotmail.com>
2024-11-29 15:14:52 -0300

    API com banco de dados aula depoloy AWS

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from 'express'
+
+import { Sequelize, DataTypes } from 'sequelize'
+
+const sequelize = new Sequelize('deploy_nodejs', 'postgres', 'teste123', {
+    host: 'deploy-nodejs.c7oi6egs4yoh.us-east-1.rds.amazonaws.com',
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
+
+const usuario = sequelize.define('Usuario', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome: DataTypes.TEXT,
+    senha: DataTypes.TEXT
+});
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/teste/:nome/:senha", async (req, res) => {
+    try {
+        await sequelize.authenticate();
+        console.log("Conexão estabelecida com o banco de dados")
+        await usuario.sync()
+        const novoUsuario = await usuario.create({
+            nome: req.params.nome,
+            senha: req.params.senha,
+        });
+        res.send("USUARIO INSERIDO", req.params.nome, req.params.senha)
+    } catch (error) {
+        res.send(error)
+        console.log("Deu merda")
+    } finally {
+        sequelize.close()
+    }
+})
+
+app.get("/", (req, res) => {
+    console.log("Bateu na API")
+    res.send("Bem vindo")
+})
+
+
+
+app.listen(3001, () => console.log("server rodando"))
