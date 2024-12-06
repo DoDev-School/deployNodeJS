7b5413138aada833af23dbf693030837045719f6
RArgollo <rafaelargollo22@hotmail.com>
2024-12-06 16:19:14 -0300

    porfavor4

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get("/teste/:nome/:senha", async (req, res) => {
 
 app.get("/", (req, res) => {
     console.log("E AGORA")
-    res.send("VAMO Q VAMO PORRA")
+    res.send("EU MUDEI")
 })
 
 
